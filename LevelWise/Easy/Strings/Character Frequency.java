c0c4e01595ed5a835a440a484fe685f354d915da
rv-work <rv.dthought@gmail.com>
2025-08-13 13:16:15 +0530

    Character frequency

---
+++ LevelWise/Easy/Strings/Character Frequency.java
@@ -0,0 +1,19 @@
+// [First Unique Character in a String](https://leetcode.com/problems/first-unique-character-in-a-string/)
+
+
+class Solution {
+    public int firstUniqChar(String s) {
+        int [] arr = new int[26];
+
+        for(int i = 0; i< s.length(); i++){
+            char c = s.charAt(i);
+            arr[c - 'a']++;
+        }
+
+        for(int i = 0; i< s.length(); i++){
+            if(arr[s.charAt(i) - 'a'] == 1) return i;
+        }
+
+        return -1;
+    }
+}

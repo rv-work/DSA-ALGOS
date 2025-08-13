07317b9ca67b25d7518d7f979fd29ef6ac7c0836
rv-work <rv.dthought@gmail.com>
2025-08-13 23:04:02 +0530

    Kadane's Algo

--- LevelWise/Medium/Arrays/Kadane's Algorithm.java
+++ LevelWise/Medium/Arrays/Kadane's Algorithm.java
@@ -1,1 +1,17 @@
 // [Maximum Subarray](https://leetcode.com/problems/maximum-subarray/)
+
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int cs = 0;
+        int ms = Integer.MIN_VALUE; 
+
+        for (int num : nums) {
+            cs += num; 
+            ms = Math.max(ms, cs); 
+            if (cs < 0) cs = 0; 
+        }
+
+        return ms;
+    }
+}

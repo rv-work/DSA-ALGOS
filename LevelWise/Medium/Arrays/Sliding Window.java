2c837e61a638e5d483afa0f5145f43875b673f11
rv-work <rv.dthought@gmail.com>
2025-08-15 13:36:36 +0530

    Sliding Window

--- LevelWise/Medium/Arrays/Sliding Window.java
+++ LevelWise/Medium/Arrays/Sliding Window.java
@@ -1,1 +1,23 @@
 //  [Longest Substring Without Repeating Characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/)
+
+
+
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int left = 0, maxLen = 0;
+        Set<Character> set = new HashSet<>();
+
+        for (int right = 0; right < n; right++) {
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(s.charAt(right));
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+}

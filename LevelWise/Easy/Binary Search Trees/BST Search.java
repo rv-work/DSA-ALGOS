a02e5e3d0ad19b007ac32ae59a776e33daae62a0
rv-work <rv.dthought@gmail.com>
2025-08-16 17:08:27 +0530

    Search In BST

---
+++ LevelWise/Easy/Binary Search Trees/BST Search.java
@@ -0,0 +1,30 @@
+// [Search in a Binary Search Tree](https://leetcode.com/problems/search-in-a-binary-search-tree/)
+
+
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        while(root != null && val != root.val)
+        root = val < root.val ? root.left : root.right;
+        
+        return root;
+    }
+}

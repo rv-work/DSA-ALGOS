88d2b9028a39dfad736b254fba06cd23d2479839
rv-work <rv.dthought@gmail.com>
2025-08-17 10:07:31 +0530

    Group Anagrams

---
+++ LevelWise/Medium/Strings/Group Anagrams.java
@@ -0,0 +1,30 @@
+//  [Group Anagrams](https://leetcode.com/problems/group-anagrams/)
+
+
+
+
+
+
+
+
+
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+            
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
+            }
+            
+            map.get(sortedWord).add(word);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+}

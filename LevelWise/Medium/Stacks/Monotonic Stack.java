126415001c952ee6b407c9b01428c2e7eaa73776
rv-work <rv.dthought@gmail.com>
2025-08-25 07:45:31 +0530

    monotonic

--- LevelWise/Medium/Stacks/Monotonic Stack.java
+++ LevelWise/Medium/Stacks/Monotonic Stack.java
@@ -27,3 +27,33 @@ class Solution {
         return ans;
     }
 }
+
+
+// mine
+
+
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int [] ans = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        Arrays.fill(ans , -1);
+
+        for (int i = 2*n - 1; i >= 0; i--) {
+            int index = i % n;
+
+            while (!st.isEmpty() && nums[st.peek()] <= nums[index]) {
+                st.pop();
+            }
+
+            if (!st.isEmpty()) {
+                ans[index] = nums[st.peek()];
+            }
+
+            st.push(index);
+        }
+
+        return ans;
+    }
+}

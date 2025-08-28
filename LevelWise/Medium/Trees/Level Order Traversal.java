28224e4367b228d62e8f53b5eb4d39749e9df0ef
rv-work <ruvishushukla1gmail.com>
2025-08-28 23:57:44 +0530

    Level Order Tr

---
+++ LevelWise/Medium/Trees/Level Order Traversal.java
@@ -0,0 +1,41 @@
+//  [Binary Tree Level Order Traversal](https://leetcode.com/problems/binary-tree-level-order-traversal/)
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        if(root == null) return ans;
+        q.add(root);
+        while(!q.isEmpty()){
+            List<Integer> level = new ArrayList<>();
+            int size = q.size();
+            while(size > 0){
+            TreeNode node = q.poll();
+            if(node.left != null) q.add(node.left);
+            if(node.right != null) q.add(node.right);
+            level.add(node.val);
+            size--;
+            }
+            ans.add(level);
+            
+        }
+
+        return ans;
+    }
+}

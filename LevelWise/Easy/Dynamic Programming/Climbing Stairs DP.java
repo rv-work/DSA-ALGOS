ba03630c22b63646e28e2a692a84487035ba1b3a
rv-work <rv.dthought@gmail.com>
2025-10-17 21:33:04 +0530

    DP - Stairs

--- LevelWise/Easy/Dynamic Programming/Climbing Stairs DP.java
+++ LevelWise/Easy/Dynamic Programming/Climbing Stairs DP.java
@@ -1,1 +1,21 @@
 // [Climbing Stairs](https://leetcode.com/problems/climbing-stairs/)
+
+
+class Solution {
+    public int climbStairs(int n) {
+        if(n <= 1) return 1;
+        
+        int prev2 = 1;
+        int prev = 2;
+
+        for(int i = 3; i <= n; i++){
+            int curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
+        }
+
+
+        return prev;
+
+    }
+}

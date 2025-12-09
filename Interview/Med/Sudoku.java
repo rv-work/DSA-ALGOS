af26f2cb1c4441a6821e401120aecb3b3f99195f
rv-work <rv.dthought@gmail.com>
2025-12-09 14:49:22 +0530

    sd-rock

--- Interview/Med/Sudoku.java
+++ Interview/Med/Sudoku.java
@@ -105,4 +105,39 @@ class Solution {
 
 
 
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+
+                if (board[i][j] == '.') continue;
+
+                char curr = board[i][j];
+
+                // Check Row
+                for (int col = 0; col < 9; col++) {
+                    if (col != j && board[i][col] == curr) return false;
+                }
+
+                // Check Column
+                for (int row = 0; row < 9; row++) {
+                    if (row != i && board[row][j] == curr) return false;
+                }
+
+                // Check 3x3 Box
+                int startRow = (i / 3) * 3;
+                int startCol = (j / 3) * 3;
+
+                for (int r = startRow; r < startRow + 3; r++) {
+                    for (int c = startCol; c < startCol + 3; c++) {
+                        if ((r != i || c != j) && board[r][c] == curr)
+                            return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
 

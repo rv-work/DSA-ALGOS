573546ba0238850333068175e22c11d66d4dd61d
rv-work <rv.dthought@gmail.com>
2025-12-09 18:48:20 +0530

    House-space

--- Interview/Med/House.java
+++ Interview/Med/House.java
@@ -42,3 +42,33 @@ class Solution {
         return dp[n-1];
     }
 }
+
+
+
+
+
+
+
+
+
+class Solution {
+    
+
+
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n == 1) return nums[0];
+        if(n == 2) return Math.max(nums[0] , nums[1]);
+        int prev2 = nums[0];
+        int prev1 = Math.max(nums[0] , nums[1]);
+
+        for(int i = 2; i< n; i++){
+            int curr = Math.max(nums[i] + prev2 , prev1);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+
+        return prev1;
+    }
+}

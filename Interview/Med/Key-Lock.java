07046ee127fcc133b437a192eb21286f637b57c6
rv-work <rv.dthought@gmail.com>
2025-12-09 14:42:09 +0530

    Key-Lock-dfs

--- Interview/Med/Key-Lock.java
+++ Interview/Med/Key-Lock.java
@@ -31,3 +31,44 @@ class Solution {
         return true;
     }
 }
+
+
+
+
+
+
+
+
+////////DFS
+
+
+class Solution {
+    
+    void dfs (boolean vis[] , int key , List<List<Integer>> rooms){
+       vis[key] = true;
+
+        for(int i = 0; i< rooms.get(key).size(); i++){
+            if(!vis[rooms.get(key).get(i)])
+            dfs(vis , rooms.get(key).get(i) ,  rooms);
+        }
+    }
+
+
+
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        boolean vis[] = new boolean[rooms.size()];
+        vis[0] = true;
+
+        for(int i = 0; i< rooms.get(0).size(); i++){
+            dfs(vis , rooms.get(0).get(i) , rooms);
+        }
+
+       
+
+        for(int i = 0; i< vis.length; i++){
+            if(!vis[i]) return false;
+        }
+
+        return true;
+    }
+}

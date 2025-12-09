dd24ecb1cdcc3c89b6b211740ebcef3b6d30df2a
rv-work <rv.dthought@gmail.com>
2025-12-09 19:21:47 +0530

    grp-anagram-opt

--- Interview/Med/AnagramGroup.java
+++ Interview/Med/AnagramGroup.java
@@ -26,3 +26,34 @@ class Solution {
         return ans;
     }
 }
+
+
+
+
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+
+            int[] freq = new int[26];
+
+            for (char c : str.toCharArray()) {
+                freq[c - 'a']++;
+            }
+
+            String key = Arrays.toString(freq);
+
+            // map.computeIfAbsent(key, k -> new ArrayList<>()).add(str);
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(str);
+
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}

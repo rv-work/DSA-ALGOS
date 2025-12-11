7a767b3f32d701042b10882cc7cd1a3c1c79d5b5
rv-work <rv.dthought@gmail.com>
2025-12-11 15:25:37 +0530

    coin-method-2d-tab

--- Interview/Med/Coin-Change.java
+++ Interview/Med/Coin-Change.java
@@ -94,3 +94,41 @@ class Solution {
         return (ans == Integer.MAX_VALUE ? -1 : ans);
     }
 }
+
+
+
+
+
+
+
+
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+
+        int[][] dp = new int[n][amount + 1];
+
+       
+        for (int t = 0; t <= amount; t++) {
+            if (t % coins[0] == 0) dp[0][t] = t / coins[0];
+            else dp[0][t] = (int)1e9;  
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int t = 0; t <= amount; t++) {
+
+                int notTake = dp[i - 1][t];
+
+                int take = (int)1e9;
+                if (coins[i] <= t)
+                    take = 1 + dp[i][t - coins[i]];  
+
+                dp[i][t] = Math.min(take, notTake);
+            }
+        }
+
+        int ans = dp[n - 1][amount];
+        return ans >= (int)1e9 ? -1 : ans;
+    }
+}

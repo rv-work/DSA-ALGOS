a17fc1335fd7c6ca022f5dbf6ccba5c859117db6
rv-work <rv.dthought@gmail.com>
2025-12-11 15:29:22 +0530

    coin-method-2d-space

--- Interview/Med/Coin-Change.java
+++ Interview/Med/Coin-Change.java
@@ -132,3 +132,45 @@ class Solution {
         return ans >= (int)1e9 ? -1 : ans;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+
+        int[] prev = new int[amount + 1];
+        int[] curr = new int[amount + 1];
+
+       
+        for (int t = 0; t <= amount; t++) {
+            if (t % coins[0] == 0) prev[t] = t / coins[0];
+            else prev[t] = (int)1e9;  
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int t = 0; t <= amount; t++) {
+
+                int notTake = prev[t];
+
+                int take = (int)1e9;
+                if (coins[i] <= t)
+                    take = 1 + curr[t - coins[i]];  
+
+                curr[t] = Math.min(take, notTake);
+            }
+            prev = curr;
+        }
+
+        int ans = prev[amount];
+        return ans >= (int)1e9 ? -1 : ans;
+    }
+}

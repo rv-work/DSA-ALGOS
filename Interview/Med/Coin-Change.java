e808cca2a91e1e53add80c8ceeb0dd60d705beb4
rv-work <rv.dthought@gmail.com>
2025-12-11 12:46:17 +0530

    coin-change-memo

--- Interview/Med/Coin-Change.java
+++ Interview/Med/Coin-Change.java
@@ -1,64 +1,31 @@
 class Solution {
 
-    int solve(int[] coins, int amt, int ind) {
+    
+
+    int solve(int[] coins, int amt , int[] dp) {
 
         if (amt == 0) return 0;
-        if (ind == coins.length) return -1;
+        if (amt < 0) return -1;
 
-        int take = -1;
-        if (coins[ind] <= amt) {
-            int res = solve(coins, amt - coins[ind], ind);
-            if (res != -1) take = 1 + res;
+        if (dp[amt] != -1) return dp[amt];
+
+        int best = Integer.MAX_VALUE;
+
+        for (int c : coins) {
+            if (c <= amt) {
+                int res = solve(coins, amt - c, dp);
+                if (res != -1) best = Math.min(best, 1 + res );
+            }
         }
 
-        int notTake = solve(coins, amt, ind + 1);
-
-        if (take == -1) return notTake;
-        if (notTake == -1) return take;
-
-        return Math.min(take, notTake);
+        dp[amt] = (best == Integer.MAX_VALUE) ? -1 : best;
+        return dp[amt];
     }
 
     public int coinChange(int[] coins, int amount) {
-        return solve(coins, amount, 0);
-    }
-}
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, -1);
 
-
-
-
-
-
-
-
-class Solution {
-    int solve(int[] coins, int amt, int ind ,  int[][] dp) {
-
-        if (amt == 0) return 0;
-        if (ind == coins.length) return -1;
-
-        if (dp[ind][amt] != -1) return dp[ind][amt];
-
-        int take = -1;
-        if (coins[ind] <= amt) {
-            int res = solve(coins, amt - coins[ind], ind , dp);
-            if (res != -1) take = 1 + res;
-        }
-
-        int notTake = solve(coins, amt, ind + 1 , dp);
-
-        if (take == -1) return dp[ind][amt] = notTake;
-        if (notTake == -1) return dp[ind][amt] = take;
-
-        return dp[ind][amt] = Math.min(take, notTake);
-    }
-
-    public int coinChange(int[] coins, int amount) {
-
-        int[][] dp = new int[coins.length][amount + 1];
-
-        for (int[] row : dp) Arrays.fill(row, -1);
-
-        return solve(coins, amount, 0 , dp);
+        return solve(coins, amount  ,dp);
     }
 }

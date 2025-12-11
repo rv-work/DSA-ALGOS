c35b8df7370c936a5e8a1b0939d1d093d77ee803
rv-work <rv.dthought@gmail.com>
2025-12-11 12:47:04 +0530

    coin-change-tab

--- Interview/Med/Coin-Change.java
+++ Interview/Med/Coin-Change.java
@@ -29,3 +29,27 @@ class Solution {
         return solve(coins, amount  ,dp);
     }
 }
+
+
+
+
+
+
+
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+
+        dp[0] = 0;
+
+        for (int c : coins) {
+            for (int amt = c; amt <= amount; amt++) {
+                dp[amt] = Math.min(dp[amt], 1 + dp[amt - c]);
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}

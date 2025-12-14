3886f88456d23913244b839470022612bd208790
rv-work <rv.dthought@gmail.com>
2025-12-14 16:57:41 +0530

    build-tree-in-pre-static

--- Interview/Med/Build-from-in-pre.java
+++ Interview/Med/Build-from-in-pre.java
@@ -41,3 +41,48 @@
 
 
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */class Solution {
+
+    static class Index {
+        int val = 0;
+    }
+
+    private TreeNode build(
+        int[] preorder,
+        int[] inorder,
+        int inStart,
+        int inEnd,
+        Index preIdx
+    ) {
+        if (inStart > inEnd) return null;
+
+        int rootVal = preorder[preIdx.val++];
+        TreeNode root = new TreeNode(rootVal);
+
+        int idx = inStart;
+        while (inorder[idx] != rootVal) idx++;
+
+        root.left  = build(preorder, inorder, inStart, idx - 1, preIdx);
+        root.right = build(preorder, inorder, idx + 1, inEnd, preIdx);
+
+        return root;
+    }
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return build(preorder, inorder, 0, inorder.length - 1, new Index());
+    }
+}

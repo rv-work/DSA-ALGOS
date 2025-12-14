8690dcc04a1c526376bcbd0b8d53a2c61673ffad
rv-work <rv.dthought@gmail.com>
2025-12-14 17:02:46 +0530

    build-tree-post-pre-with-map-to-reduce-index-find-time

--- Interview/Med/build-tree-post-pre.java
+++ Interview/Med/build-tree-post-pre.java
@@ -51,3 +51,71 @@
         return build(preorder, postorder, 0, postorder.length - 1);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    int preIdx = 0;
+    HashMap<Integer, Integer> postMap = new HashMap<>();
+
+    private TreeNode build(
+        int[] preorder,
+        int[] postorder,
+        int postStart,
+        int postEnd
+    ) {
+        if (preIdx >= preorder.length || postStart > postEnd)
+            return null;
+
+        TreeNode root = new TreeNode(preorder[preIdx++]);
+
+        if (postStart == postEnd)
+            return root;
+
+        int leftRootVal = preorder[preIdx];
+        int idx = postMap.get(leftRootVal);
+
+        root.left = build(preorder, postorder, postStart, idx);
+        root.right = build(preorder, postorder, idx + 1, postEnd - 1);
+
+        return root;
+    }
+
+    public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+
+        for (int i = 0; i < postorder.length; i++) {
+            postMap.put(postorder[i], i);
+        }
+
+        return build(preorder, postorder, 0, postorder.length - 1);
+    }
+}

9792be5133a259d78d84a6c0b38335fe96fc0d4d
rv-work <rv.dthought@gmail.com>
2025-12-15 19:07:54 +0530

    longest repeating replacement

---
+++ Interview/Med/Longest Repeating Replacement.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+
+        int n = s.length();
+        int l = 0;
+        int maxLen = 0;
+
+        Map<Character, Integer> mp = new HashMap<>();
+
+        for (int r = 0; r < n; r++) {
+            char c = s.charAt(r);
+            mp.put(c, mp.getOrDefault(c, 0) + 1);
+
+            while (true) {
+
+                int total = 0;
+                int Lmax = 0;
+
+                for (int val : mp.values()) {
+                    Lmax = Math.max(Lmax, val);
+                    total += val;
+                }
+
+                int needToChange = total - Lmax;
+
+                if (needToChange <= k) break;
+
+                char leftChar = s.charAt(l);
+                mp.put(leftChar, mp.get(leftChar) - 1);
+                if (mp.get(leftChar) == 0)
+                    mp.remove(leftChar);
+                l++;
+            }
+
+            maxLen = Math.max(maxLen, r - l + 1);
+        }
+
+        return maxLen;
+    }
+}

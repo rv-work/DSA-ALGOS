ba9cd63825c1c151768521bd3b6b0caa69e3abf7
rv-work <rv.dthought@gmail.com>
2025-12-15 19:54:06 +0530

    max occ of subs set

--- Interview/Med/Max Occ Oa A Subs.java
+++ Interview/Med/Max Occ Oa A Subs.java
@@ -137,3 +137,38 @@ class Solution {
         return ans;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+    public int maxFreq(String s, int maxLetters, int minSize, int maxSize) {
+        Map<String, Integer> freq=new HashMap<>();
+        int n=s.length();
+        int max=0;
+        for(int i=0;i<=n-minSize;i++){
+            String str=s.substring(i,i+minSize);
+            if(helper(str,maxLetters)){
+                freq.put(str,freq.getOrDefault(str,0)+1);
+                max=Math.max(max,freq.get(str));
+            }
+        }
+        return max;
+    }
+    private boolean helper(String str,int max){
+        Set<Character> set=new HashSet<>();
+        for(char c:str.toCharArray()){
+            set.add(c);
+            if(set.size()>max)return false;
+        }
+        return true;
+    }
+}

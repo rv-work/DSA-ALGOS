313a72fc0fa4de781564e6e7ef2e4389b5ddc5f7
rv-work <rv.dthought@gmail.com>
2025-12-15 23:39:10 +0530

    Coin Chaneg ||

---
+++ Interview/Med/Coin Change 2.java
@@ -0,0 +1,18 @@
+class Solution {
+
+
+    int ans(int [] coins , int amt , int idx){
+       if(amt == 0) return 1;
+       if(idx < 0) return 0;
+
+       int take = 0;
+       if(amt >= coins[idx]) take = ans(coins , amt - coins[idx] , idx);
+       int notTake = ans(coins , amt  , idx-1);
+
+       return take + notTake;
+    }
+
+    public int change(int amount, int[] coins) {
+        return ans(coins , amount , coins.length - 1);
+    }
+}

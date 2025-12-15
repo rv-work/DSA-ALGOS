6e122a2493c340a8643ac6d7753d3bc5fb8db23d
rv-work <rv.dthought@gmail.com>
2025-12-15 19:16:02 +0530

    longest repeating replacement - maxFreq Global Never Dec

--- Interview/Med/Longest Repeating Replacement.java
+++ Interview/Med/Longest Repeating Replacement.java
@@ -38,3 +38,40 @@ class Solution {
         return maxLen;
     }
 }
+
+
+
+
+
+
+
+
+
+class Solution {
+    public int characterReplacement(String s, int k) {
+
+        int n = s.length();
+        int l = 0;
+        int maxLen = 0;
+
+        Map<Character, Integer> mp = new HashMap<>();
+        int maxFreq = 0;  
+
+        for (int r = 0; r < n; r++) {
+            char c = s.charAt(r);
+            mp.put(c, mp.getOrDefault(c, 0) + 1);
+
+            maxFreq = Math.max(maxFreq, mp.get(c));
+
+            while ((r - l + 1) - maxFreq > k) {
+                char leftChar = s.charAt(l);
+                mp.put(leftChar, mp.get(leftChar) - 1);
+                l++;
+            }
+
+            maxLen = Math.max(maxLen, r - l + 1);
+        }
+
+        return maxLen;
+    }
+}

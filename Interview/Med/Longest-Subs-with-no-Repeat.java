7021d905aece6139623a255ec8598819aeb0648e
rv-work <rv.dthought@gmail.com>
2025-12-15 18:41:12 +0530

    longest subs with no duplicate- set

--- Interview/Med/Longest-Subs-with-no-Repeat.java
+++ Interview/Med/Longest-Subs-with-no-Repeat.java
@@ -1,28 +1,57 @@
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        
-        int n = s.length();
-        if(n <= 1) return n;
-        int l = 0; int r = 0;
-        int max = Integer.MIN_VALUE;
-        Map<Character , Integer> map = new HashMap<>();
-        while(r < n){
-           char ch = s.charAt(r);
-           if(!map.containsKey(ch)){
-               map.put(ch , 1);
-           } else {
-             while(ch != s.charAt(l)){
-                map.remove(s.charAt(l++));
-             } 
-             l++;
-           }
+  public int lengthOfLongestSubstring(String s) {
 
-           max = Math.max(max , r-l+1);
-           r++;
+    int n = s.length();
+    if (n <= 1)
+      return n;
+    int l = 0;
+    int r = 0;
+    int max = Integer.MIN_VALUE;
+    Map<Character, Integer> map = new HashMap<>();
+    while (r < n) {
+      char ch = s.charAt(r);
+      if (!map.containsKey(ch)) {
+        map.put(ch, 1);
+      } else {
+        while (ch != s.charAt(l)) {
+          map.remove(s.charAt(l++));
         }
+        l++;
+      }
 
-
-
-        return max;
+      max = Math.max(max, r - l + 1);
+      r++;
     }
+
+    return max;
+  }
+}
+
+class Solution {
+  public int lengthOfLongestSubstring(String s) {
+
+    int n = s.length();
+    if (n <= 1)
+      return n;
+    int l = 0;
+    int r = 0;
+    int max = Integer.MIN_VALUE;
+    Set<Character> set = new HashSet<>();
+    while (r < n) {
+      char ch = s.charAt(r);
+      if (!set.contains(ch)) {
+        set.add(ch);
+      } else {
+        while (ch != s.charAt(l)) {
+          set.remove(s.charAt(l++));
+        }
+        l++;
+      }
+
+      max = Math.max(max, r - l + 1);
+      r++;
+    }
+
+    return max;
+  }
 }

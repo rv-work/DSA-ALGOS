7ad7ba8566c1d7499f0d149cfa710e13744848e3
rv-work <rv.dthought@gmail.com>
2025-12-15 23:42:38 +0530

    Coin Chaneg || - memo

--- Interview/Med/Coin Change 2.java
+++ Interview/Med/Coin Change 2.java
@@ -14,5 +14,32 @@ class Solution {
 
     public int change(int amount, int[] coins) {
         return ans(coins , amount , coins.length - 1); 
+
+    }
+}
+
+
+
+
+
+
+class Solution {
+
+
+    int ans(int [] coins , int amt , int idx , int[][] dp){
+       if(amt == 0) return 1;
+       if(idx < 0) return 0;
+       if(dp[idx][amt] != -1) return dp[idx][amt];
+       int take = 0;
+       if(amt >= coins[idx]) take = ans(coins , amt - coins[idx] , idx , dp);
+       int notTake = ans(coins , amt  , idx-1  , dp);
+
+       return dp[idx][amt] =  take + notTake;
+    }
+
+    public int change(int amount, int[] coins) {
+        int [][] dp = new int[coins.length][amount+1];
+        for(int nums []: dp ) Arrays.fill(nums , -1);
+        return ans(coins , amount , coins.length - 1 , dp);
     }
 }

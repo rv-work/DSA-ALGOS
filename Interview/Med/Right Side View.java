eca62bdc747181230baea3ae17dbb0421e1ee576
rv-work <rv.dthought@gmail.com>
2025-12-16 17:30:49 +0530

    Binary Tree Right Side View -BFS

--- Interview/Med/Right Side View.java
+++ Interview/Med/Right Side View.java
@@ -13,51 +13,29 @@
  *     }
  * }
  */
-// class Solution {
-//     public List<Integer> rightSideView(TreeNode root) {
-//          List<Integer> result = new ArrayList<>();
-
-//        if(root == null)return result;
-
-//        Queue<TreeNode> queue = new LinkedList<>();
-//        queue.offer(root); 
-
-//        while(!queue.isEmpty()){
-//         int levelsize = queue.size();
-//         for(int i = 0; i < levelsize ; i++){
-//             TreeNode currentNode = queue.poll();
-//             if (i == levelsize -1 ) result.add(currentNode.val);
-//             if(currentNode.left != null) queue.offer(currentNode.left);
-//             if(currentNode.right != null) queue.offer(currentNode.right);
-//         }
-        
-//        }
-//        return result;
-//     }
-// }
 
 
 
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-      List<Integer> res = new ArrayList<>();
+         List<Integer> result = new ArrayList<>();
 
-      recursionRight(root, 0, res);
+       if(root == null)return result;
 
-      return res;
-    }
+       Queue<TreeNode> queue = new LinkedList<>();
+       queue.offer(root); 
 
-     private void recursionRight(TreeNode root, int level, List<Integer> res) {
-       
-        if (root == null) return;
-
-        if (res.size() == level) 
-            res.add(root.val);
-            
-            recursionRight(root.right, level + 1, res);
-            recursionRight(root.left, level + 1, res);
+       while(!queue.isEmpty()){
+        int levelsize = queue.size();
+        for(int i = 0; i < levelsize ; i++){
+            TreeNode currentNode = queue.poll();
+            if (i == levelsize -1 ) result.add(currentNode.val);
+            if(currentNode.left != null) queue.offer(currentNode.left);
+            if(currentNode.right != null) queue.offer(currentNode.right);
+        }
         
-
-     }
-
+       }
+       return result;
+    }
 }
+

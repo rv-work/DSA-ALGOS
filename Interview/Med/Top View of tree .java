31a121bd907eef1796aff4a2d8d1ea9eb0320865
rv-work <rv.dthought@gmail.com>
2025-12-16 18:29:20 +0530

    top view of tree - pair one queue

--- Interview/Med/Top View of tree .java
+++ Interview/Med/Top View of tree .java
@@ -50,3 +50,71 @@ class Solution {
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+// User function Template for Java
+
+/*
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+*/
+class Pair {
+    int hd;
+    Node node;
+    
+    Pair(Node node , int hd){
+        this.node = node ;
+        this.hd = hd;
+    }
+}
+
+class Solution {
+    
+    
+    static ArrayList<Integer> topView(Node root) {
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        Queue<Pair> q = new LinkedList<>();
+        Map<Integer , Integer> mp = new TreeMap<>();
+        if(root == null) return ans;
+        
+        q.add(new Pair(root , 0));
+        
+        while(!q.isEmpty()){
+            Pair curr = q.poll();
+            
+            if(!mp.containsKey(curr.hd)){
+              mp.put(curr.hd , curr.node.data);
+            } 
+            
+            if(curr.node.left != null){
+                q.add(new Pair(curr.node.left , curr.hd - 1));
+            }
+            
+            if(curr.node.right != null){
+                q.add(new Pair(curr.node.right , curr.hd + 1));
+            }
+        }   
+        
+        for(int val : mp.values()){
+            ans.add(val);
+        }
+        
+        return ans;
+    }
+}

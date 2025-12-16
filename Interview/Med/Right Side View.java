8f15b1e8b702b7a7766c985b7312b1e7e08fd9b8
rv-work <rv.dthought@gmail.com>
2025-12-16 17:31:05 +0530

    Binary Tree Right Side View - DFS

--- Interview/Med/Right Side View.java
+++ Interview/Med/Right Side View.java
@@ -39,3 +39,33 @@ class Solution {
     }
 }
 
+
+
+
+
+
+
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+      List<Integer> res = new ArrayList<>();
+
+      recursionRight(root, 0, res);
+
+      return res;
+    }
+
+     private void recursionRight(TreeNode root, int level, List<Integer> res) {
+       
+        if (root == null) return;
+
+        if (res.size() == level) 
+            res.add(root.val);
+            
+            recursionRight(root.right, level + 1, res);
+            recursionRight(root.left, level + 1, res);
+        
+
+     }
+
+}

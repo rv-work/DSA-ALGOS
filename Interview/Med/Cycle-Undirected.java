a10312ae9eec3f44b2f1d6d71c43748087a08e31
rv-work <rv.dthought@gmail.com>
2025-12-16 22:59:49 +0530

    cycle in undirected bfs

--- Interview/Med/Cycle-Undirected.java
+++ Interview/Med/Cycle-Undirected.java
@@ -37,3 +37,68 @@ class Solution {
         return false;
     }
 }
+
+
+
+
+
+
+
+
+
+
+class Solution {
+
+    public boolean isCycle(int V, int[][] edges) {
+
+        boolean[] vis = new boolean[V];
+
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++) adj.add(new ArrayList<>());
+
+        for (int[] e : edges) {
+            adj.get(e[0]).add(e[1]);
+            adj.get(e[1]).add(e[0]);
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        int [] par = new int[V];
+        
+       
+        
+        for(int k = 0; k< V; k++){
+            if(!vis[k]){
+                 q.add(k);
+                 vis[k] = true;
+                 par[k] = -1;
+                 
+                 
+                 while(!q.isEmpty()){
+                 int size = q.size();
+                 
+                    for(int i = 0; i< size; i++){
+                        
+                        int node = q.poll();
+                        vis[node] = true;
+                        
+                        for(int nei : adj.get(node)){
+                            
+                            if(vis[nei] && nei != par[node]) return true;
+                            
+                            if(!vis[nei]){
+                                q.add(nei);
+                                par[nei] = node;
+                            }
+                        }
+                    }
+                 }
+        
+
+            }
+        }
+        
+        
+        
+        return false;
+    }
+}

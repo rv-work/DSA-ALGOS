c2d6216462d381e79f48c6ef56fe835ad7a518c7
rv-work <rv.dthought@gmail.com>
2025-12-17 17:05:18 +0530

    freq of most freq -

--- Interview/Med/Freq of most frequent.java
+++ Interview/Med/Freq of most frequent.java
@@ -22,3 +22,34 @@ class Solution {
         return ans;
     }
 }
+
+
+
+
+
+
+
+
+class Solution {
+    public int maxFrequency(int[] nums, int k) {
+
+        Arrays.sort(nums);   
+
+        long total = 0;     
+        int l = 0;
+        int ans = 1;
+
+        for (int r = 0; r < nums.length; r++) {
+            total += nums[r];
+
+            while ((long)nums[r] * (r - l + 1) - total > k) {
+                total -= nums[l];
+                l++;
+            }
+
+            ans = Math.max(ans, r - l + 1);
+        }
+
+        return ans;
+    }
+}

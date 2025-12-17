02e588a7cab862e6931dfad7c0adfe4edcf93d2c
rv-work <rv.dthought@gmail.com>
2025-12-17 12:04:00 +0530

    count good subarrays - mine

--- Interview/Med/Count Good SubArrays.java
+++ Interview/Med/Count Good SubArrays.java
@@ -24,3 +24,57 @@ class Solution {
         return ans;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+    public long countGood(int[] nums, int k) {
+        Map<Integer, Integer> mp = new HashMap<>();
+        int n = nums.length;
+        int l = 0, r = 0;
+        long cnt = 0;
+        long ans = 0;
+
+        while (r < n) {
+
+            mp.put(nums[r], mp.getOrDefault(nums[r], 0) + 1);
+            if (mp.get(nums[r]) >= 2)
+                cnt += mp.get(nums[r]) - 1;
+
+            while (cnt >= k) {
+                if (mp.get(nums[l]) >= 2)
+                    cnt -= mp.get(nums[l]) - 1;
+                mp.put(nums[l], mp.get(nums[l]) - 1);
+                l++;
+            }
+
+            ans += l;
+
+            r++;
+
+        }
+
+        return ans;
+
+    }
+}

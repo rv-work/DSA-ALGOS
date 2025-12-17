59627fc60d1fef31a8f73421d3ad81e79a06cf7f
rv-work <rv.dthought@gmail.com>
2025-12-17 12:59:10 +0530

    count good subarrays - rec memo using map

--- Interview/Med/Min Operations to Reduce X.java
+++ Interview/Med/Min Operations to Reduce X.java
@@ -26,3 +26,50 @@ class Solution {
         return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 }
+
+
+
+
+
+
+
+
+
+class Solution {
+
+    Map<String, Integer> dp = new HashMap<>();
+
+    int sol(int[] nums, int x, int i, int j) {
+
+        if (x == 0) return 0;
+        if (x < 0) return Integer.MAX_VALUE;
+        if (i > j) return Integer.MAX_VALUE;
+
+        String key = i + "|" + j + "|" + x;
+        if (dp.containsKey(key)) return dp.get(key);
+
+        int takeI = Integer.MAX_VALUE;
+        int takeJ = Integer.MAX_VALUE;
+
+        if (nums[i] <= x) {
+            int res = sol(nums, x - nums[i], i + 1, j);
+            if (res != Integer.MAX_VALUE)
+                takeI = 1 + res;
+        }
+
+        if (nums[j] <= x) {
+            int res = sol(nums, x - nums[j], i, j - 1);
+            if (res != Integer.MAX_VALUE)
+                takeJ = 1 + res;
+        }
+
+        int ans = Math.min(takeI, takeJ);
+        dp.put(key, ans);
+        return ans;
+    }
+
+    public int minOperations(int[] nums, int x) {
+        int ans = sol(nums, x, 0, nums.length - 1);
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+}

23ea2f95c4557f04cc2ae0f9e2ecb6c83ab6d6be
rv-work <rv.dthought@gmail.com>
2025-12-17 17:04:53 +0530

    freq of most freq - mine

--- Interview/Med/Min Operations to Reduce X.java
+++ Interview/Med/Min Operations to Reduce X.java
@@ -202,27 +202,3 @@ class Solution {
 
 
 
-class Solution {
-    public int maxFrequency(int[] nums, int k) {
-
-        Arrays.sort(nums);  
-
-        int l = 0;
-        long totalDiff = 0;     
-        int ans = 1;
-
-        for (int r = 1; r < nums.length; r++) {
-
-            totalDiff += (long)(nums[r] - nums[r - 1]) * (r - l);
-
-            while (totalDiff > k) {
-                totalDiff -= nums[r] - nums[l];
-                l++;
-            }
-
-            ans = Math.max(ans, r - l + 1);
-        }
-
-        return ans;
-    }
-}

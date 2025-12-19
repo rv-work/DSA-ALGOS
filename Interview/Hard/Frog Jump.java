b73fa329e19d21d8bf31fb52db289f57179d4afc
rv-work <rv.dthought@gmail.com>
2025-12-19 11:08:55 +0530

    Frog Jump -rec - better way

--- Interview/Hard/Frog Jump.java
+++ Interview/Hard/Frog Jump.java
@@ -94,3 +94,79 @@ class Solution {
 }
 
 
+
+
+class Solution {
+
+  boolean ans(int[] stones ,  int idx , int lastJumped , 
+           Map<Long , Integer> mp , Map<Long , Boolean> [] dp) {
+
+   if(idx == stones.length - 1) 
+       return true;
+
+   if(dp[idx].containsKey((long) lastJumped)) 
+       return dp[idx].get((long) lastJumped);
+
+   boolean op1 = false;
+   boolean op2 = false;
+   boolean op3 = false;
+
+   // lastJumped - 1
+   if(lastJumped - 1 > 0)
+   {
+       long nextStone = stones[idx] + lastJumped - 1;
+       if(mp.containsKey(nextStone))
+       {
+           int nextIdx = mp.get(nextStone);
+           if(nextIdx > idx)
+               op1 = ans(stones , nextIdx , lastJumped - 1 , mp , dp);
+       }
+   }
+
+   // lastJumped
+   {
+       long nextStone = stones[idx] + lastJumped;
+       if(mp.containsKey(nextStone))
+       {
+           int nextIdx = mp.get(nextStone);
+           if(nextIdx > idx)
+               op2 = ans(stones , nextIdx , lastJumped , mp , dp);
+       }
+   }
+
+   // lastJumped + 1
+   {
+       long nextStone = stones[idx] + lastJumped + 1;
+       if(mp.containsKey(nextStone))
+       {
+           int nextIdx = mp.get(nextStone);
+           if(nextIdx > idx)
+               op3 = ans(stones , nextIdx , lastJumped + 1 , mp , dp);
+       }
+   }
+
+   boolean ans = op1 || op2 || op3;
+
+   dp[idx].put((long) lastJumped , ans);
+
+   return ans;
+}
+
+public boolean canCross(int[] stones) {
+
+   int n = stones.length;
+
+  if (stones[1] - stones[0] > 1)
+ return false;
+
+   Map<Long, Boolean>[] dp = new HashMap[n];
+   for (int i = 0; i < n; i++)
+       dp[i] = new HashMap<>();
+
+   Map<Long, Integer> mp = new HashMap<>();
+   for (int i = 0; i < n; i++)
+       mp.put((long) stones[i], i);
+
+   return ans(stones, 1, 1, mp, dp);
+}
+}

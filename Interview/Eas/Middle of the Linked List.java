b657ac6b4c67f5334edbc82f6902415237a6f585
rv-work <rv.dthought@gmail.com>
2025-12-21 06:23:00 +0530

    Middle of the linkedlist - another way

--- Interview/Eas/Middle of the Linked List.java
+++ Interview/Eas/Middle of the Linked List.java
@@ -1,21 +1,53 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
-    public ListNode middleNode(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        while(fast != null && fast.next != null ){
-            fast = fast.next.next;
-            slow = slow.next;
-        }    
-      return slow;
+  public ListNode middleNode(ListNode head) {
+    ListNode slow = head;
+    ListNode fast = head;
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
     }
+    return slow;
+  }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+  public ListNode middleNode(ListNode head) {
+    // 1. Find length
+    int len = 0;
+    ListNode temp = head;
+    while (temp != null) {
+      len++;
+      temp = temp.next;
+    }
+
+    // 2. Go till half (len/2)
+    int mid = len / 2;
+    temp = head;
+    while (mid-- > 0) {
+      temp = temp.next;
+    }
+
+    return temp;
+  }
 }

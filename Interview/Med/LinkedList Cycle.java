8938a25fc0b82ef0c261f6f4f0440f5459c9d53f
rv-work <rv.dthought@gmail.com>
2025-12-21 06:55:16 +0530

    LinkedList Cycle ||-better

--- Interview/Med/LinkedList Cycle.java
+++ Interview/Med/LinkedList Cycle.java
@@ -28,3 +28,98 @@ public class Solution {
         return null; // no cycle
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head == null || head.next == null) return null;
+        
+        ListNode slow = head;
+        ListNode fast = head;
+
+        // Phase 1: detect if cycle exists
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){ 
+                break;
+            }
+        }
+
+        // no cycle
+        if(fast == null || fast.next == null) 
+            return null;
+
+        // Phase 2: move slow to head, keep fast at meeting point
+        slow = head;
+        while(slow != fast){
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow;  // cycle start node
+    }
+}
+
+
+
+
+
+
+
+// a = distance from head to cycle start
+
+// b = distance from cycle start to meeting point
+
+// c = remaining cycle distance to return to cycle start
+
+// Cycle length = b + c
+
+// 🔥 Step 1: Meeting point kaise banta hai?
+
+// Slow moves 1 step,
+// Fast moves 2 steps,
+// So fast 2x speed pe hai.
+
+// At meeting:
+
+// distance slow ran  = a + b  
+// distance fast ran  = a + b + n(b + c)
+
+
+// fast slow se n cycles zyada daudta hai.
+
+// So:
+
+// 2(a + b) = a + b + n(b + c)
+
+
+// Solve:
+
+// a + b = n(b + c)
+// a = n(b + c) − b
+// a = (n − 1)(b + c) + c

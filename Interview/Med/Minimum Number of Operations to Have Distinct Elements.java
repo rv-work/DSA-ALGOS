39d1f8126733db8937df8f3e3ebacce7bd1399d9
rv-work <rv.dthought@gmail.com>
2025-12-21 16:06:02 +0530

    Minimum Number of Operations to Have Distinct Elements

---
+++ Interview/Med/Minimum Number of Operations to Have Distinct Elements.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int n = nums.length;
+        int i = n-1; 
+        Set<Integer> st = new HashSet<>();
+        
+        while(i >= 0){
+            if(st.contains(nums[i])) break;
+            st.add(nums[i]);
+            i--;
+        }
+        
+        if(i < 0) return 0;
+
+        int without = n - 1 - i;  
+
+        int tooRemove = i + 1;
+
+        int div = tooRemove / 3;
+
+        if(tooRemove % 3 == 0) return div;
+        else return div + 1;
+    }
+}

13a0bd1788fa41781c95943def9beb6bc934fda3
rv-work <rv.dthought@gmail.com>
2025-12-21 06:35:43 +0530

    LinkedList Cycle-better

--- Interview/Eas/LinkedList Cycle.java
+++ Interview/Eas/LinkedList Cycle.java
@@ -24,3 +24,44 @@ public class Solution {
         return false;
     }
 }
+
+
+
+
+
+
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+ 
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+
+      ListNode cur = head;
+    int steps = 10001;
+
+    while(cur != null && steps-- > 0){
+        cur = cur.next;
+    }
+    return cur != null; // true means cycle
+    }
+}
+
+
+
+
+
+// cycle means null tak kabhi phuchengi hi nhi 

fd02c3e767c40448caed9c1f21834bdbaaced5ad
rv-work <rv.dthought@gmail.com>
2025-12-28 22:06:08 +0530

    ALl Ones + 1

--- Interview/Med/Smallest All-Ones Multiple.java
+++ Interview/Med/Smallest All-Ones Multiple.java
@@ -14,3 +14,21 @@ class Solution {
         return -1;
     }
 }
+
+
+
+
+
+
+
+class Solution {
+    public int minAllOneMultiple(int k) {
+        if(k % 2 == 0 || k % 5 == 0) return - 1;
+        int rem = 0;
+        for(int i = 1; i <= k; i++) {
+            rem = (rem * 10 + 1) % k;
+            if(rem == 0) return i;
+        }
+        return -1;
+    }
+}

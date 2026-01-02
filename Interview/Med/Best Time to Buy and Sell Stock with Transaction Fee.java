e15ed613acc9f6943989c6b4e26bb45ca599b9cc
rv-work <rv.dthought@gmail.com>
2026-01-02 22:38:05 +0530

    Stock...-memo

--- Interview/Med/Best Time to Buy and Sell Stock with Transaction Fee.java
+++ Interview/Med/Best Time to Buy and Sell Stock with Transaction Fee.java
@@ -1,20 +1,51 @@
 class Solution {
 
-    int rec(int[] prices, int idx, boolean canBuy, int fee) {
-        if (idx >= prices.length) return 0;
+  int rec(int[] prices, int idx, boolean canBuy, int fee) {
+    if (idx >= prices.length)
+      return 0;
 
-        if (canBuy) {
-            int buy = -prices[idx] - fee + rec(prices, idx + 1, false, fee);
-            int notBuy = rec(prices, idx + 1, true, fee);
-            return Math.max(buy, notBuy);
-        } else {
-            int sell = prices[idx]  + rec(prices, idx + 1, true, fee);
-            int notSell = rec(prices, idx + 1, false, fee);
-            return Math.max(sell, notSell);
-        }
+    if (canBuy) {
+      int buy = -prices[idx] - fee + rec(prices, idx + 1, false, fee);
+      int notBuy = rec(prices, idx + 1, true, fee);
+      return Math.max(buy, notBuy);
+    } else {
+      int sell = prices[idx] + rec(prices, idx + 1, true, fee);
+      int notSell = rec(prices, idx + 1, false, fee);
+      return Math.max(sell, notSell);
+    }
+  }
+
+  public int maxProfit(int[] prices, int fee) {
+    return rec(prices, 0, true, fee);
+  }
+}
+
+class Solution {
+
+  int rec(int[] prices, int idx, int canBuy, int fee, int[][] dp) {
+    if (idx >= prices.length)
+      return 0;
+
+    if (dp[idx][canBuy] != -1)
+      return dp[idx][canBuy];
+
+    if (canBuy == 1) {
+      int buy = -prices[idx] + rec(prices, idx + 1, 0, fee, dp);
+      int notBuy = rec(prices, idx + 1, 1, fee, dp);
+      dp[idx][canBuy] = Math.max(buy, notBuy);
+    } else {
+      int sell = prices[idx] - fee + rec(prices, idx + 1, 1, fee, dp);
+      int notSell = rec(prices, idx + 1, 0, fee, dp);
+      dp[idx][canBuy] = Math.max(sell, notSell);
     }
 
-    public int maxProfit(int[] prices, int fee) {
-        return rec(prices, 0, true, fee);
-    }
+    return dp[idx][canBuy];
+  }
+
+  public int maxProfit(int[] prices, int fee) {
+    int[][] dp = new int[prices.length][2];
+    for (int[] num : dp)
+      Arrays.fill(num, -1);
+    return rec(prices, 0, 1, fee, dp);
+  }
 }

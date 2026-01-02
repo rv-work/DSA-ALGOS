6cf82bbc31828659ea6a6fc9375c8f032b477f87
rv-work <rv.dthought@gmail.com>
2026-01-02 23:05:35 +0530

    Best Time to Buy and Sell Stock with Cooldown- memo

--- Interview/Med/Best Time to Buy and Sell Stock with Cooldown.java
+++ Interview/Med/Best Time to Buy and Sell Stock with Cooldown.java
@@ -19,3 +19,40 @@ class Solution {
         return rec(prices, 0, true);
     }
 }
+
+
+
+
+
+
+
+
+class Solution {
+
+    Integer[][] dp;
+
+    int rec(int[] prices, int idx, int canBuy) {
+        if (idx >= prices.length) return 0;
+
+        if (dp[idx][canBuy] != null) return dp[idx][canBuy];
+
+        int ans;
+        if (canBuy == 1) {
+            int buy = -prices[idx] + rec(prices, idx + 1, 0);
+            int skip = rec(prices, idx + 1, 1);
+            ans = Math.max(buy, skip);
+        } else {
+            int sell = prices[idx] + rec(prices, idx + 2, 1); // cooldown
+            int hold = rec(prices, idx + 1, 0);
+            ans = Math.max(sell, hold);
+        }
+
+        return dp[idx][canBuy] = ans;
+    }
+
+    public int maxProfit(int[] prices) {
+        dp = new Integer[prices.length][2];
+        return rec(prices, 0, 1);
+    }
+}
+

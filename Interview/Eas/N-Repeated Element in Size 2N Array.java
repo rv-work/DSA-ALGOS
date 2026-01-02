b064600411384025ae2c760fa61c9de4dedbe384
rv-work <rv.dthought@gmail.com>
2026-01-02 17:57:29 +0530

    N-repeated elements - another way.......6th"

--- Interview/Eas/N-Repeated Element in Size 2N Array.java
+++ Interview/Eas/N-Repeated Element in Size 2N Array.java
@@ -79,3 +79,21 @@ class Solution {
 
 
 
+
+
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] == nums[j]) {
+                    return nums[i];
+                }
+            }
+        }
+        return -1;
+    }
+}
+
+

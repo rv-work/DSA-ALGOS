52d7d9d735c209ca991b79ead806cec639bf6614
rv-work <rv.dthought@gmail.com>
2026-01-06 14:54:10 +0530

    POTD-06-01-2026

--- Interview/Med/POTD3.java
+++ Interview/Med/POTD3.java
@@ -42,3 +42,64 @@ class Solution {
     return ans;
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */class Solution {
+
+    void dfs(TreeNode node, int level, List<Long> sum) {
+        if (node == null) return;
+
+        if (level == sum.size()) {
+            sum.add((long) node.val);
+        } else {
+            sum.set(level, sum.get(level) + node.val);
+        }
+
+        dfs(node.left, level + 1, sum);
+        dfs(node.right, level + 1, sum);
+    }
+
+    public int maxLevelSum(TreeNode root) {
+        List<Long> sum = new ArrayList<>();
+
+        dfs(root, 0, sum);
+
+        long maxSum = Long.MIN_VALUE;
+        int ans = 1;
+
+        for (int i = 0; i < sum.size(); i++) {
+            if (sum.get(i) > maxSum) {
+                maxSum = sum.get(i);
+                ans = i + 1; // 1-based level
+            }
+        }
+
+        return ans;
+    }
+}
